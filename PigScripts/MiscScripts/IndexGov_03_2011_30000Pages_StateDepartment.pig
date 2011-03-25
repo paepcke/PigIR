4fcbf6d97c8ab4544d4ed5f591de8c19b7bba9e4
Andreas Paepcke <paepcke@cs.stanford.edu>
2011-03-25 11:31:07 -0700

    Two state department scripts.

---
+++ PigScripts/MiscScripts/IndexGov_03_2011_30000Pages_StateDepartment.pig
@@ -0,0 +1,21 @@
+REGISTER $PIG_HOME/contrib/piggybank/java/piggybank.jar;
+REGISTER contrib/PigIRWithSource.jar;
+
+docs = LOAD 'gov-03-2011:30000:www.state.gov:www.state.gov'
+	   USING pigir.webbase.WebBaseLoader()
+	   AS (
+	   		url:chararray,
+			date:chararray,
+			pageSize:int,
+			position:int,
+			docidInCrawl:int,
+			httpHeader:chararray,
+			content:chararray);
+		
+rawIndex = FOREACH docs GENERATE 
+		    pigir.pigudf.IndexOneDoc(pigir.pigudf.GetLUID(), content);
+		    
+flatRawIndex = FOREACH rawIndex GENERATE flatten($0);
+index = ORDER flatRawIndex BY token ASC, docID ASC PARALLEL 5; 
+STORE index INTO 'Datasets/gov-03-2011-wwwStateGov-30000Pages-index.csv'
+			USING PigStorage(',');
---
+++ src/pigtests/TestIndexOneDocument.java
@@ -0,0 +1,139 @@
+package pigtests;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.pig.ExecType;
+import org.apache.pig.PigServer;
+import org.apache.pig.backend.executionengine.ExecException;
+
+import pigir.Common;
+
+public class TestIndexOneDocument {
+	
+	Properties props = new Properties();
+	PigServer pserver = null;
+
+	public TestIndexOneDocument() {
+		try {
+			props.setProperty("pig.usenewlogicalplan", "false");
+			pserver = new PigServer(ExecType.MAPREDUCE, props);
+		} catch (ExecException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	public void doTest0() {
+		
+		try {
+
+			Map<String, String> env = System.getenv();
+			URI piggybankPath = new File(env.get("PIG_HOME"),
+					"contrib/piggybank/java/piggybank.jar").toURI();
+			pserver.registerJar(piggybankPath.toString());
+			pserver.registerJar("contrib/PigIR.jar");
+
+			pserver.registerQuery(
+					"docs = LOAD '2006-08:2' " +    // Gary test
+					"USING pigir.webbase.WebBaseLoader() " +
+					"AS (url:chararray, " +
+					"	 date:chararray, " +
+					"	 pageSize:int, " +
+					"	 position:int, " +
+					"	 docidInCrawl:int, " +
+					"	 httpHeader:chararray, " +
+					"	 content:chararray);"
+			);
+			Common.print(pserver, "docs");
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void doTest1() {
+		try {
+			props.setProperty("pig.usenewlogicalplan", "false");
+			pserver = new PigServer(ExecType.MAPREDUCE, props);
+
+			Map<String, String> env = System.getenv();
+			URI piggybankPath = new File(env.get("PIG_HOME"),
+					"contrib/piggybank/java/piggybank.jar").toURI();
+			pserver.registerJar(piggybankPath.toString());
+			pserver.registerJar("contrib/PigIR.jar");
+
+			pserver.registerQuery(
+					//"docs = LOAD '2006-08:2' " +    // Gary test
+					"docs = LOAD 'gov-03-2011:30000' " +
+					"USING pigir.webbase.WebBaseLoader() " +
+					"AS (url:chararray, " +
+					"	 date:chararray, " +
+					"	 pageSize:int, " +
+					"	 position:int, " +
+					"	 docidInCrawl:int, " +
+					"	 httpHeader:chararray, " +
+					"	 content:chararray);"
+			);
+			//Common.print(pserver, "docs");
+			pserver.registerQuery(
+					"rawIndex = FOREACH docs GENERATE" +
+					"           pigir.pigudf.IndexOneDoc(pigir.pigudf.GetLUID(), content);"
+			);
+
+			pserver.registerQuery(
+					"flatRawIndex = FOREACH rawIndex GENERATE flatten($0) AS (token:chararray, docID:chararray, tokenPos:int);"
+			);
+
+			pserver.registerQuery(
+					//"index = ORDER flatRawIndex BY token ASC, docID ASC PARALLEL 5;"
+					"index = ORDER flatRawIndex BY token ASC;"
+			);
+
+			pserver.registerQuery(
+					"STORE index INTO 'Datasets/gov-03-2011-wwwStateGov-30000Pages-index.csv' " +
+					//"STORE flatRawIndex INTO 'Datasets/gov-03-2011-wwwStateGov-30000Pages-index.csv' " +
+                        "USING PigStorage(',');"
+			);	
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void doTests2() {
+		try {
+
+			Map<String, String> env = System.getenv();
+			URI piggybankPath = new File(env.get("PIG_HOME"),
+					"contrib/piggybank/java/piggybank.jar").toURI();
+			pserver.registerJar(piggybankPath.toString());
+			pserver.registerJar("contrib/PigIR.jar");
+
+			pserver.registerQuery(
+					"flatRawIndex = LOAD 'Datasets/gov-03-2011-wwwStateGov-30000Pages-index.csv'" +
+					"USING PigStorage(',')" +
+					"AS (token:chararray, docID:chararray, tokenPos:int);"
+			);
+			
+			pserver.registerQuery(
+					"index = ORDER flatRawIndex BY token ASC, docID ASC PARALLEL 5;"
+			);			
+			
+			pserver.registerQuery(
+					"STORE index INTO 'Datasets/gov-03-2011-indexSorted.csv' " +
+                        "USING PigStorage(',');"
+			);	
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new TestIndexOneDocument().doTest1();
+		//new TestIndexOneDocument().doTests2();
+	}
+
+}
