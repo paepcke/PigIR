aa579c6dff79ba0af6d4f6302aadaec4bf7eace7
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-01-09 18:21:25 -0800

    Added  Pig filter function CSVFieldsNumeric. Added comments to ISNumber.

--- src/main/java/edu/stanford/pigir/pigudf/CSVFieldsNumeric.java
+++ src/main/java/edu/stanford/pigir/pigudf/CSVFieldsNumeric.java
@@ -1,21 +1,21 @@
 package edu.stanford.pigir.pigudf;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 
-import org.apache.pig.FilterFunc;
 import org.apache.pig.backend.executionengine.ExecException;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 
 /**
  * @author paepcke
- * Pig Filter function: Receives a string and returns true 
- * if it is purely numeric. Recognizes hyphen, plus, and minus
- * as strings. 
+ * Pig Filter function: Receives a CSV string and returns true 
+ * if all fields are purely numeric. Recognizes hyphen, plus, and minus
+ * as strings.  
  */
-public class IsNumber extends FilterFunc {
+public class CSVFieldsNumeric {
+	
+	
 	public Boolean exec(Tuple input) throws IOException {
 		if (input == null || input.size() == 0)
 			return null;
@@ -34,7 +34,7 @@ public class IsNumber extends FilterFunc {
     
 	public static boolean isNumeric(String str) {
 	    for (char c : str.toCharArray()) {
-	        if (c != '.' && c != '-' && c != '+' & !Character.isDigit(c)) return false;
+	        if (c!= ',' && c != '.' && c != '-' && c != '+' & !Character.isDigit(c)) return false;
 	    }
 	    return true;
 	}    
@@ -46,7 +46,7 @@ public class IsNumber extends FilterFunc {
 	public static void main(String[] args) throws IOException {
 		Tuple testTuple;
 		
-		IsNumber numChecker = new IsNumber();
+		CSVFieldsNumeric numChecker = new CSVFieldsNumeric();
 		ArrayList<String> input = new ArrayList<String>();
 		input.add("10");
 		testTuple = TupleFactory.getInstance().newTuple(input);
@@ -72,11 +72,23 @@ public class IsNumber extends FilterFunc {
 			throw new IOException("-100 was not recognized as a number.");
 		}
 		input.clear();
+		input.add("100,399");
+		testTuple = TupleFactory.getInstance().newTuple(input);
+		if (!numChecker.exec(testTuple)) {
+			throw new IOException("100,399 was not recognized as a number.");
+		}
+		input.clear();
+		input.add("foo,399");
+		testTuple = TupleFactory.getInstance().newTuple(input);
+		if (numChecker.exec(testTuple)) {
+			throw new IOException("foo,399 was not recognized as a number.");
+		}
+		input.clear();
 		input.add("foo.bar");
 		testTuple = TupleFactory.getInstance().newTuple(input);
 		if (numChecker.exec(testTuple)) {
 			throw new IOException("recognized foo.bar as a number.");
 		}
-		System.out.println("All good with IsNumeric.");
+		System.out.println("All good with CSVFieldsNumeric.");
 	}
 }
