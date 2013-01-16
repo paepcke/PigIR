d76458dd73b4b9e4279d951784cd9b33efcdf1ae
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-01-15 16:44:29 -0800

    Added unit tests for WARC/1.0 and for gzipped files.

--- src/test/java/edu/stanford/pigir/warc/WarcRecordReaderTest.java
+++ src/test/java/edu/stanford/pigir/warc/WarcRecordReaderTest.java
@@ -17,18 +17,23 @@ public class WarcRecordReaderTest {
 	TaskAttemptContextForTesting taskAttemptContext;
 	FileSplit inputSplit;
 	File testWarcFile0_18;
+	File testWarcFile0_18GZipped;
 	File testWarcFile1_0;
 	WarcRecordReader warcReader0_18;
 	WarcRecordReader warcReader1_0;
+	WarcRecordReader warcReader0_18GZipped;
 	
 	@Before
 	public void setUp() throws Exception {
+		
+		//---------- WARC/0.18 -------------
+		
 		testWarcFile0_18 = new File("src/test/resources/tinyWarc0_18.warc");
 		if (!testWarcFile0_18.exists()) {
 			System.out.println("File " + testWarcFile0_18 + " does not exist.");
 		} else {
-			System.out.println("File " + testWarcFile0_18 + " exists.");
-			System.out.println("File length is " + testWarcFile0_18.length());
+			//System.out.println("File " + testWarcFile0_18 + " exists.");
+			//System.out.println("File length is " + testWarcFile0_18.length());
 		}
 		
 		String[] emptyHostStrArray = new String[0];
@@ -38,13 +43,14 @@ public class WarcRecordReaderTest {
 		warcReader0_18 = new WarcRecordReader();
 		warcReader0_18.initialize(inputSplit, taskAttemptContext);
 		
-		// WARC 1.0:
+		//---------- WARC/1.0 -------------
+
 		testWarcFile1_0 = new File("src/test/resources/tinyWarc1_0.warc");
 		if (!testWarcFile1_0.exists()) {
 			System.out.println("File " + testWarcFile1_0 + " does not exist.");
 		} else {
-			System.out.println("File " + testWarcFile1_0 + " exists.");
-			System.out.println("File length is " + testWarcFile1_0.length());
+			//System.out.println("File " + testWarcFile1_0 + " exists.");
+			//System.out.println("File length is " + testWarcFile1_0.length());
 		}
 		
 		inputSplit = new FileSplit(new Path(testWarcFile1_0.getAbsolutePath()), (long)0, testWarcFile1_0.length(), emptyHostStrArray);
@@ -53,8 +59,20 @@ public class WarcRecordReaderTest {
 		warcReader1_0 = new WarcRecordReader();
 		warcReader1_0.initialize(inputSplit, taskAttemptContext);
 		
+		//---------- WARC/0.18GZipped -------------		
+		testWarcFile0_18GZipped = new File("src/test/resources/tinyWarc0_18.warc.gz");
+		if (!testWarcFile0_18GZipped.exists()) {
+			System.out.println("File " + testWarcFile0_18GZipped + " does not exist.");
+		} else {
+			//System.out.println("File " + testWarcFile0_18GZipped + " exists.");
+			//System.out.println("File length is " + testWarcFile0_18GZipped.length());
+		}
 		
+		inputSplit = new FileSplit(new Path(testWarcFile0_18GZipped.getAbsolutePath()), (long)0, testWarcFile0_18GZipped.length(), emptyHostStrArray);
+		taskAttemptContext = new TaskAttemptContextForTesting();
 		
+		warcReader0_18GZipped = new WarcRecordReader();
+		warcReader0_18GZipped.initialize(inputSplit, taskAttemptContext);
 	}
 
 	@Test
@@ -80,6 +98,31 @@ public class WarcRecordReaderTest {
 		int len2ndContentRecord = 21032; // Includes HTTP header
 		assertEquals(len2ndContentRecord, Integer.parseInt(record.get(WarcRecord.CONTENT_LENGTH)));
 	}
+
+	@Test
+	public void testWarc0_18GZipped() throws IOException {
+		assertTrue(warcReader0_18GZipped.nextKeyValue());
+		LongWritable key = warcReader0_18GZipped.getCurrentKey();
+		WarcRecord   record = warcReader0_18GZipped.getCurrentValue();
+		assertEquals(0, key.get());
+		assertEquals("warcinfo", record.get(WarcRecord.WARC_TYPE));
+		//assertEquals("WARC/0.18", record.get(WarcRecord.WARC_TYPE));
+		
+		assertTrue(warcReader0_18GZipped.nextKeyValue());
+		record = warcReader0_18GZipped.getCurrentValue();
+		assertEquals("response", record.get(WarcRecord.WARC_TYPE));
+		int lenFirstContentRecord = 21064; // Includes HTTP header
+		assertEquals(lenFirstContentRecord, Integer.parseInt(record.get(WarcRecord.CONTENT_LENGTH)));
+		String content = record.get(WarcRecord.CONTENT);
+		assertEquals(lenFirstContentRecord, content.length());
+		
+		assertTrue(warcReader0_18GZipped.nextKeyValue());
+		record = warcReader0_18GZipped.getCurrentValue();
+		assertEquals("response", record.get(WarcRecord.WARC_TYPE));
+		int len2ndContentRecord = 21032; // Includes HTTP header
+		assertEquals(len2ndContentRecord, Integer.parseInt(record.get(WarcRecord.CONTENT_LENGTH)));
+	}
+	
 	
 	@Test
 	public void testWarc1_0() throws IOException {
@@ -88,5 +131,14 @@ public class WarcRecordReaderTest {
 		WarcRecord   record = warcReader1_0.getCurrentValue();
 		assertEquals(0, key.get());
 		assertEquals("warcinfo", record.get(WarcRecord.WARC_TYPE));
+
+		assertTrue(warcReader1_0.nextKeyValue());
+		record = warcReader1_0.getCurrentValue();
+		int lenFirstContentRecord = 62; // Includes HTTP header
+		assertEquals(lenFirstContentRecord, Integer.parseInt(record.get(WarcRecord.CONTENT_LENGTH)));
+		String content = record.get(WarcRecord.CONTENT);
+		assertEquals(lenFirstContentRecord, content.length());
+		
+	
 	}
 }
