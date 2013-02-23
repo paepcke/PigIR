d77182d5d71df93d12e79dd1205150c4447f048a
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-02-23 13:05:39 -0800

    Continued on nohaddoop, standalone WARC stream reader.

--- src/main/java/edu/stanford/pigir/warc/nohadoop/WarcRecordReader.java
+++ src/main/java/edu/stanford/pigir/warc/nohadoop/WarcRecordReader.java
@@ -1,28 +1,20 @@
 package edu.stanford.pigir.warc.nohadoop;
 
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.zip.GZIPInputStream;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import edu.stanford.pigir.pigudf.LineAndChunkReader;
-//import org.apache.log4j.Logger;
 
 
 /**
  * Treats keys as offset wbRecordReader file and value as one Warc record. 
  */
-public class WarcRecordReader extends RecordReader<LongWritable, Text> {
+public class WarcRecordReader {
 	
   private static final boolean DO_READ_CONTENT = true;
   private static final int DEFAULT_BUFFER_SIZE = 64 * 1024;
@@ -31,100 +23,62 @@ public class WarcRecordReader extends RecordReader<LongWritable, Text> {
   private long end;
   private LineAndChunkReader warcLineReader;
   private DataInputStream warcInStream; 
-  private LongWritable keyWarcStreamPos = null;
+  private long keyWarcStreamPos = (long) 0;
   private WarcRecord valueWarcRecord = null;
-  private FSDataInputStream fileIn = null;
+  private FileInputStream fileIn = null;
   private Logger logger=null;
 
-    
-  public void initialize(InputSplit genericSplit,
-                         TaskAttemptContext context) throws IOException {
-	logger = new Logger();
-    FileSplit split = (FileSplit) genericSplit;
-    Configuration job = context.getConfiguration();
-
-    start = split.getStart();
-    end = start + split.getLength();
-    final Path file = split.getPath();
-    FileSystem fs = file.getFileSystem(job);
-    fileIn = fs.open(split.getPath());
-
-    try {
-    	GZIPInputStream gzWarcInStream = new GZIPInputStream(fileIn,
-    													   job.getInt("io.file.buffer.size", DEFAULT_BUFFER_SIZE));
-    	warcInStream = new DataInputStream(gzWarcInStream);
-    } catch (IOException e) {
-    	// Not a gzipped file?
-    	try {
-    		// The attempt to read using the gzip stream above consumed
-    		// the first two bytes. Reset:
-    		fileIn.seek(0);
-    		warcInStream = new DataInputStream (fileIn);
-    	} catch (Exception e1) {
-    		logger.info("Could not open WARC split.");
-    		return;
-    	}
-    }
-    // TODO: Test slices with non-zero start, and with start > MAX_INTEGER
-    boolean skipFirstLine = false;
-    if (start != 0) {
-    	skipFirstLine = true;
-    	--start;
-    	// Skipping wbRecordReader a datastream only works for int. So, if
-    	// start within the slice is greater than what fits into 
-    	// an int, we need to skip wbRecordReader multiple steps:
-    	if (start > Integer.MAX_VALUE) {
-    		for (int intChunkCount=0; intChunkCount < (start / Integer.MAX_VALUE); intChunkCount++) {
-    			warcInStream.skipBytes(Integer.MAX_VALUE);
-    		}
-    		warcInStream.skipBytes(((Long)(start % (new Long(Integer.MAX_VALUE)))).intValue());
-    	}
-    	warcInStream.skipBytes((int) (start - pos));
-    }
-    warcLineReader = new LineAndChunkReader(warcInStream, job);
-
-    if (skipFirstLine) {  // skip first line and re-establish "start".
-      start += warcLineReader.readLine(new Text(), 0,
-                           (int)Math.min((long)Integer.MAX_VALUE, end - start));
-    }
-    this.pos = start;
+  public WarcRecordReader(File warcPath) {
+	  try {
+		  fileIn = new FileInputStream(warcPath);
+		  GZIPInputStream gzWarcInStream = new GZIPInputStream(fileIn,DEFAULT_BUFFER_SIZE);
+	      warcInStream = new DataInputStream(gzWarcInStream);
+	  } catch (IOException e) {
+		  // Not a gzipped file?
+		  try {
+			  // The attempt to read using the gzip stream above consumed
+			  // the first two bytes. Reset:
+			  fileIn = new FileInputStream(warcPath);
+			  warcInStream = new DataInputStream (fileIn);
+		  } catch (Exception e1) {
+			  logger.info("Could not open WARC file.");
+			  return;
+		  }
+	  }
+	  warcLineReader = new LineAndChunkReader(warcInStream);
+	  start = 0;
+	  pos = 0;
   }
-  
-  
+    
   public boolean nextKeyValue() throws IOException {
 	  return nextKeyValue(DO_READ_CONTENT);
   }
   
   public boolean nextKeyValue(boolean readContents) throws IOException {
-    if (keyWarcStreamPos == null) {
-      keyWarcStreamPos = new LongWritable();
-    }
-    keyWarcStreamPos.set(pos);
+    keyWarcStreamPos = pos;
     valueWarcRecord = WarcRecord.readNextWarcRecord(warcLineReader, readContents);
     if (valueWarcRecord == null) {
-    	keyWarcStreamPos = null;
+    	keyWarcStreamPos = 0;
     	return false;
     }
     
-    logger.debug("Pulled another WARC record.");
+    //System.out.println("Pulled another WARC record.");
     
     // Update position wbRecordReader the Data stream
     pos += valueWarcRecord.getTotalRecordLength();
     return true;
   }
 
-  @Override
-  public LongWritable getCurrentKey() {
+  public long getCurrentKey() {
     return keyWarcStreamPos;
   }
 
-  @Override
   public WarcRecord getCurrentValue() {
     return valueWarcRecord;
   }
 
   /**
-   * Get the progress within the split
+   * Get the progress within the file:
    */
   public float getProgress() {
     if (start == end) {
